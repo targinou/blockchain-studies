edc469255b7c7b01a1068c5a54b7f285b93cf023
Emanuel Targino <emanuel.targino.700@ufrn.edu.br>
2022-07-13 12:27:23 -0300

    first deploy

---
+++ WEB3_PY_SIMPLE_STORAGE/deploy.py
@@ -0,0 +1,23 @@
+import imp
+from solcx import compile_standard, install_solc
+
+install_solc("0.6.0")
+
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {"*": ["abi", "metadata", "evm.bytecode", "evm.sourceMap"]}
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+print(compiled_sol)

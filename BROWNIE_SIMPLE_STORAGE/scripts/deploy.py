ce8a3922f94480bab95925baf74388ccffdefa02
targinou <emanueltargino1@gmail.com>
2022-07-15 00:44:58 -0300

    finalizando lição 5

--- BROWNIE_SIMPLE_STORAGE/scripts/deploy.py
+++ BROWNIE_SIMPLE_STORAGE/scripts/deploy.py
@@ -1,13 +1,25 @@
-from brownie import accounts, config, SimpleStorage
-import os
+from turtle import update
+from brownie import accounts, config, SimpleStorage, network
 
 
 def deploy_simple_storage():
-    account = accounts[0]
+    account = get_account()
     simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_store_value = simple_storage.retrieve()
+    print(updated_store_value)
+
+
+def get_account():
+    if (network.show_active) == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
+
     # account = accounts.load("my-account")
-    # account = accounts.add(config["wallets"]["from_key"])
-    print(simple_storage)
 
 
 def main():
